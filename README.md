8f0ed12882aaf0cefab986368c01ff6a52d4b698
MuffinLinwist <carlosbarrientosugarte97@gmail.com>
2022-08-16 13:35:44 +0200

    Updated bowernnumerals

---
+++ README.md
@@ -0,0 +1,34 @@
+# CLDF dataset derived from Bowern et al.'s "Diversity in the Numeral Systems of Australian Languages" from 2012
+
+## How to cite
+
+If you use these data please cite
+- the original source
+  > Bowern, Claire, and Jason Zentz (2012): Diversity in the Numeral Systems of Australian Languages. Anthropological Linguistics, 2012. http://www.jstor.org/stable/23621076
+- the derived dataset using the DOI of the [particular released version](../../releases/) you were using
+
+## Description
+
+
+This dataset is licensed under a CC-BY-4.0 license
+
+Available online at https://www.jstor.org/stable/23621076
+
+## Statistics
+
+
+![Glottolog: 0%](https://img.shields.io/badge/Glottolog-0%25-red.svg "Glottolog: 0%")
+![Concepticon: 0%](https://img.shields.io/badge/Concepticon-0%25-red.svg "Concepticon: 0%")
+![Source: 0%](https://img.shields.io/badge/Source-0%25-red.svg "Source: 0%")
+
+- **Varieties:** 0
+- **Concepts:** 0
+- **Lexemes:** 0
+- **Sources:** 0
+- **Synonymy:** 0.00
+
+## CLDF Datasets
+
+The following CLDF datasets are available in [cldf](cldf):
+
+- CLDF [Wordlist](https://github.com/cldf/cldf/tree/master/modules/Wordlist) at [cldf\cldf-metadata.json](cldf\cldf-metadata.json)
--- lexibank_bowernnumerals.py
+++ lexibank_bowernnumerals.py
@@ -1,4 +1,5 @@
 import pathlib
+from tkinter.tix import COLUMN
 import attr
 from clldutils.misc import slug
 from pylexibank import Dataset as BaseDataset
@@ -6,6 +7,7 @@ from pylexibank import progressbar as pb
 from pylexibank import Language
 from pylexibank import FormSpec
 from csvw.dsv import UnicodeDictReader
+
 
 @attr.s
 class CustomLanguage(Language):
@@ -19,7 +21,7 @@ class Dataset(BaseDataset):
     id = "bowernnumerals"
     languageclass = CustomLanguage
     form_spec = FormSpec(
-        separators="", missing_data=["?"], first_forms_only=True,
+        separators="", missing_data=["?"], first_form_only=True,
     )
 
     def cmd_makecldf(self, args):
@@ -35,7 +37,7 @@ class Dataset(BaseDataset):
                 Name=concept["ENGLISH"],
                 Concepticon_ID=concept["CONCEPTICON_ID"],
                 Concepticon_Gloss=concept["CONCEPTICON_GLOSS"],
-            )
+                )
         args.log.info("added concepts")
 
         languages = args.writer.add_languages(lookup_factory="NameInSource")
@@ -43,6 +45,22 @@ class Dataset(BaseDataset):
             language["NameInSource"]: language["Sources"].strip().split(",")
             for language in self.languages}
         args.log.info("added languages")
+        errors = set()
+        
 
-        for row in self.raw_dir.read_csv("data.tsv", delimiter = "\t", dicts=True):
 
+        data = self.raw_dir.read_csv(
+            "data.tsv", delimiter="\t"
+        )
+        header = data[0]
+        for i in range(len(data)):
+            entries = dict(zip(header, data[i]))
+            for language, lidx in languages.items():
+                if row["LANGUAGE"] not in languages:
+                    errors.add(("language", row["LANGUAGES"]))
+                else:
+                    args.writer.add_forms_from_value(
+                        Language_ID = lidx, 
+                        Parameter_ID = concepts[concept],
+                        Value = entries,
+                        )
